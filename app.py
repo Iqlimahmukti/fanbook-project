e890afa713c0f2441d3902b4b4d98f3ec09115e7
Iqlimahmukti <iqlimahmuktinugroho12@gmail.com>
2023-04-11 21:41:32 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-
 db = client[DB_NAME]
 
 app = Flask(__name__)
@@ -21,22 +20,40 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    # sample_receive = request.form['sample_give']
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+    count = db.bucket.count_documents({})
+    num = count + 1 
     doc = {
-        'name': name_receive,
-        'comment': comment_receive,
+        'num': num, 
+        'bucket': bucket_receive,
+        'done': 0
     }
-    db.fanmessages.insert_one(doc)
-    return jsonify({'msg':'Comment Posted!'})
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': 'data saved!'})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    message_list = list(db.fanmessages.find({}, {'_id': False}))
-    return jsonify({'messages': message_list})
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form["num_give"]
+    db.bucket.update_one(
+        {'num': int(num_receive)},
+        {'$se': {'done': 1}}
+    )
+    return jsonify({'msg': 'Update Done!'})
+
+@app.route("/bucket/delete", methods=["POST"])
+def bucket_delete():
+    num_receive = request.form["num_give"]
+    db.bucket.delete_one(
+        {'num': int(num_receive)}
+    )
+    return jsonify({'msg': 'Delete Success!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'bucket':'buckets_list'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
